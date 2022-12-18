2421580ee0055cbc0a22b33c3400ae9568642a58
HalynaB <galja2908@gmail.com>
2022-12-18 12:21:44 +0100

    meanings of the word
    I added the meanings to my dictionary search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,17 @@
-import React, {useState} from "react";
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
-
-    function handleResponse(response) {
-      console.log(response.data[0]);
-    }
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -20,8 +21,9 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange}/>
+        <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results}/>
     </div>
   );
 }

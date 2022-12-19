49895a063d698fae73a85c8ea7617e0d9c77086c
HalynaB <galja2908@gmail.com>
2022-12-19 16:41:10 +0100

    Photos
    I added photos to my Dictionary app

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,31 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
-  function handleResponse(response) {
+  let [photos, setPhotos] = useState(null);
+
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
-function search() {
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-  axios.get(apiUrl).then(handleResponse);
-}
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleDictionaryResponse);
+    const pexelsApiKey =
+      "563492ad6f917000010000014de058717bb34865a837a26800bb4078";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Autorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -29,26 +41,28 @@ function search() {
     setLoaded(true);
     search();
   }
-  if(loaded) {
+  if (loaded) {
     return (
       <div className="Dictionary">
         <section>
           <h1>What do you want to look up?</h1>
-        <form onSubmit={handleSubmit}>
-          <input type="search" onChange={handleKeywordChange} 
-          defaultValue={props.defaultKeyword}/>
-        </form>
-        <div className="hint">
-          suggested words: sunset, sky, train, plant..
-        </div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">
+            suggested words: sunset, sky, train, plant..
+          </div>
         </section>
-        <Results results={results}/>
+        <Results results={results} />
+        <Photos photos={photos}/>
       </div>
     );
-  }
-  else {
+  } else {
     load();
-    return "Loading..."
+    return "Loading...";
   }
-
 }

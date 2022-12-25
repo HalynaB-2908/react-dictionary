f911b22a2d7eb7771981e9bf061592c5067f45dd
HalynaB <galja2908@gmail.com>
2022-12-25 16:48:51 +0100

    logo picture
    I added logo picture from unDraw to my project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from "./undraw_engineering_team_a7n2.svg";
 import './App.css';
 import Dictionary from "./Dictionary";
 function App() {
@@ -5,7 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-       
+      <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
       <main>
         <Dictionary defaultKeyword="bag"/>
